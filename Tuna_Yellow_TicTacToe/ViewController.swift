c9d1e599a93f08a6a0f51c9baf0654be55532bf2
Tuna Ayberk Ozmen <tunaayberk@hotmail.com>
2019-05-29 15:19:56 -0400

    Initial Commit

---
+++ Tuna_Yellow_TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tuna_Yellow_TicTacToe
+//
+//  Created by Tuna Ayberk Ozmen on 2019-05-29.
+//  Copyright © 2019 Tuna Ayberk Ozmen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tuna_Yellow_TicTacToeTests/Tuna_Yellow_TicTacToeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tuna_Yellow_TicTacToeTests.swift
+//  Tuna_Yellow_TicTacToeTests
+//
+//  Created by Tuna Ayberk Ozmen on 2019-05-29.
+//  Copyright © 2019 Tuna Ayberk Ozmen. All rights reserved.
+//
+
+import XCTest
+@testable import Tuna_Yellow_TicTacToe
+
+class Tuna_Yellow_TicTacToeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
